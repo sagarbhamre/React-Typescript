d23d7b2a0583837111c43533d33e13c30b5e9054
sagarbhmr@gmail.com <sagarbhr@gmail.com>
2024-09-04 16:46:54 +0530

    initial commit

--- dist/analytics/analytics.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-console.log('Sending data for test....');
-function sendAnalytics(data) {
-    console.log(data);
-}
-sendAnalytics('test');
-//# sourceMappingURL=analytics.js.map
--- src/analytics/analytics.ts
+++
@@ -1,7 +0,0 @@
-console.log('Sending data for test....');
-
-function sendAnalytics(data){
-    console.log(data);
-}
-
-sendAnalytics('test');
