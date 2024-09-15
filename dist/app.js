223c8dd2fa2e410a6fe6b77d9bbc2303d3c185d1
sagarbhmr@gmail.com <sagarbhr@gmail.com>
2024-09-16 00:15:14 +0530

    Discriminated Unions

--- dist/app.js
+++ dist/app.js
@@ -43,4 +43,16 @@ function useVehicle(vehicle) {
 }
 useVehicle(v1);
 useVehicle(v2);
+function moveAnimal(animal) {
+    let speed;
+    switch (animal.type) {
+        case 'bird':
+            speed = animal.flyingSpeed;
+            break;
+        case 'horse':
+            speed = animal.runningSpeed;
+    }
+    console.log('Moving at speed: ' + speed);
+}
+moveAnimal({ type: 'bird', flyingSpeed: 77 });
 //# sourceMappingURL=app.js.map
