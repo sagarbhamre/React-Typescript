b4d650df63c2739ae85f9109d29d7bab80e56d93
sagarbhmr@gmail.com <sagarbhr@gmail.com>
2024-09-15 23:56:30 +0530

    type guards

--- dist/app.js
+++ dist/app.js
@@ -4,4 +4,43 @@ const e1 = {
     privileges: ['create-server'],
     startDate: new Date()
 };
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log('Name: ' + emp.name);
+    if ('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('Start Date: ' + emp.startDate);
+    }
+}
+printEmployeeInformation(e1);
+class Car {
+    drive() {
+        console.log('Driving...');
+    }
+}
+class Truck {
+    drive() {
+        console.log('Driving truck');
+    }
+    loadCargo(amount) {
+        console.log('Loading cargo ...' + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(7);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
 //# sourceMappingURL=app.js.map
