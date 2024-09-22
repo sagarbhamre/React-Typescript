fb2adbfdb74e32b953fdd41f9defc9cc251a1bfd
sagarbhmr@gmail.com <sagarbhr@gmail.com>
2024-09-22 08:44:36 +0530

    generics

--- dist/app.js
+++ dist/app.js
@@ -1,25 +1,3 @@
 "use strict";
-var _a;
-const e1 = {
-    name: 'Sagar',
-    privileges: ['create-server'],
-    startDate: new Date()
-};
-function add(a, b) {
-    if (typeof a === 'string' || typeof b === 'string') {
-        return a.toString() + b.toString();
-    }
-    return a + b;
-}
-const result = add('Sagar', ' Bunty');
-console.log(result);
-const fetchedUserData = {
-    id: 'u1',
-    name: 'Sagar',
-    job: { title: 'CEO', description: 'My Own Company' }
-};
-console.log((_a = fetchedUserData === null || fetchedUserData === void 0 ? void 0 : fetchedUserData.job) === null || _a === void 0 ? void 0 : _a.title);
-const userInput = '';
-const storedData = userInput !== null && userInput !== void 0 ? userInput : 'DEFAULT';
-console.log(storedData);
+const names = ['Sagar', 'Bunty'];
 //# sourceMappingURL=app.js.map
